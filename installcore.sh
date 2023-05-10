750d5941328b4e6c38f77613e3e05a8d2f634dd1
Fa-r27-aD <82215397+Fa-r27-aD@users.noreply.github.com>
2023-05-10 22:31:46 +0330

    Update and rename owo.sh to installcore.sh

--- installcore.sh
+++ installcore.sh
@@ -3,10 +3,10 @@
 sleep 5
 service passwall stop
 cd /tmp
-wget https://hoverco.net/xray-core_1.8.1-1_mipsel_24kc.ipk
+wget https://downloads.openwrt.org/releases/22.03.5/packages/mipsel_24kc/packages/xray-core_1.8.1-1_mipsel_24kc.ipk
 opkg install xray-core_1.8.1-1_mipsel_24kc.ipk -d ram
 rm xray-core_1.8.1-1_mipsel_24kc.ipk
 service passwall restart
 cd /root/
 
-echo "OK !"
+echo "That's OK!"
