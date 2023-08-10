bf01cdb4eb3d28d248b00d08f943ea82258d5d38
Fa-r27-aD <82215397+Fa-r27-aD@users.noreply.github.com>
2023-08-10 10:16:13 +0330

    Update installcore.sh

--- installcore.sh
+++ installcore.sh
@@ -4,9 +4,9 @@ sleep 5
 service passwall stop
 service passwall2 stop
 cd /tmp
-wget https://downloads.openwrt.org/releases/22.03.5/packages/mipsel_24kc/packages/xray-core_1.8.1-1_mipsel_24kc.ipk
-opkg install xray-core_1.8.1-1_mipsel_24kc.ipk -d ram
-rm xray-core_1.8.1-1_mipsel_24kc.ipk
+wget https://downloads.openwrt.org/releases/22.03.5/packages/mipsel_24kc/packages/xray-core_1.8.3-1_mipsel_24kc.ipk
+opkg install xray-core_1.8.3-1_mipsel_24kc.ipk -d ram
+rm xray-core_1.8.3-1_mipsel_24kc.ipk
 service passwall restart
 service passwall2 restart
 cd /root/
