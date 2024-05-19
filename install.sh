ecb8cee7fdea662bd79ef149c177b010c9b32e35
Fa-r27-aD <82215397+Fa-r27-aD@users.noreply.github.com>
2024-05-19 17:10:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,13 +38,6 @@ sleep 2
 opkg install trojan-plus_10.0.3-2_mipsel_24kc.ipk
 sleep 2
 
-# Download and install luci-app-passwall2 package
-wget https://github.com/xiaorouji/openwrt-passwall2/releases/download/1.29-1/luci-23.05_luci-app-passwall2_1.29-1_all.ipk
-opkg install luci-23.05_luci-app-passwall2_1.29-1_all.ipk
-
-# Clean up the downloaded IPK files
-rm *.ipk
-
 # Download the installcore script
 wget https://raw.githubusercontent.com/Fa-r27-aD/Passwall-on-Archer-C6-v3.20/main/installcore.sh
 chmod 777 installcore.sh
@@ -59,5 +52,15 @@ chmod +x runservice
 
 echo "Starting Service..."
 /etc/init.d/runservice start
+
+# Change to the root directory
+cd /root/
+
+# Download and install luci-app-passwall2 package
+wget https://github.com/xiaorouji/openwrt-passwall2/releases/download/1.29-1/luci-23.05_luci-app-passwall2_1.29-1_all.ipk
+opkg install luci-23.05_luci-app-passwall2_1.29-1_all.ipk
+
+# Clean up the downloaded files
+rm *
 
 echo "Done!"
