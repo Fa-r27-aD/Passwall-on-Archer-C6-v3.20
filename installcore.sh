e121aa6e991f64e718b3f0f595826dfc4ae6c92a
Fa-r27-aD <82215397+Fa-r27-aD@users.noreply.github.com>
2024-05-19 15:31:42 +0330

    Update installcore.sh

--- installcore.sh
+++ installcore.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+# Sleep for 5 seconds to ensure any previous tasks are completed
 sleep 5
+
+# Stop the passwall and passwall2 services
 service passwall stop
 service passwall2 stop
+
+# Change to the /tmp directory
 cd /tmp
+
+# Download the xray-core package
 wget https://mirror-03.infra.openwrt.org/releases/23.05.3/packages/mipsel_24kc/packages/xray-core_1.8.11-1_mipsel_24kc.ipk
+
+# Install the xray-core package to the RAM directory
 opkg install xray-core_1.8.11-1_mipsel_24kc.ipk -d ram
+
+# Remove the downloaded package file to clean up
 rm xray-core_1.8.11-1_mipsel_24kc.ipk
+
+# Restart the passwall and passwall2 services
 service passwall restart
 service passwall2 restart
-cd /root/
 
-echo "That's OK!"
+# Change back to the /root directory
+cd /root/
